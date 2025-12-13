d2b6e3f91da450c1e75c3f5d827bfa51bc2b7fe2
jade fisher <fisherrjd@gmail.com>
2025-12-13 11:25:03 -0700

    init

---
+++ src/main/java/rip/jade/partytimeserverjava/PartyTimeServerJavaApplication.java
@@ -0,0 +1,13 @@
+package rip.jade.partytimeserverjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyTimeServerJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartyTimeServerJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rip/jade/partytimeserverjava/PartyTimeServerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package rip.jade.partytimeserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyTimeServerJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
